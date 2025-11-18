60f6cf3b8463286ab90d91da702d79653fe3c09c
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:20 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Like a king/index.html', imgSrc: 'like a king.jpeg', name: 'Like a king' , 'page': 5, 'id': 910 },
   { href: './html/One trick mage/index.html', imgSrc: 'one trick mage.jpeg', name: 'One trick mage' , 'page': 5, 'id': 912 },
   { href: './html/Orcs vs elves/index.html', imgSrc: 'orcs vs elves.jpeg', name: 'Orcs vs elves' , 'page': 5, 'id': 924 },
   { href: './html/Rodha/index.html', imgSrc: 'rodha.jpeg', name: 'Rodha' , 'page': 5, 'id': 931 },
