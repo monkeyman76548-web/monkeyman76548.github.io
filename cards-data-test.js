4184e83c0c770816c5b1eea77b07599477a81419
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:49:52 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -2,7 +2,6 @@ export default [
 { href: './html/Buckshot Roulette/index.html', imgSrc: 'buckshot roulette.jpeg', name: 'Buckshot Roulette' , 'page': 5, 'id': 763 },
 { href: './html/Swords and sandals/index.html', imgSrc: 'swords and sandals.jpeg', name: 'Swords and sandals' , 'page': 5, 'id': 999 },
 { href: './html/Road of the dead/index.html', imgSrc: 'road of the dead.jpeg', name: 'Road of the dead' , 'page': 5, 'id': 995 },
-{ href: './html/Island Clash/index.html', imgSrc: 'island clash.jpeg', name: 'Island Clash' , 'page': 5, 'id': 994 },
 { href: './html/SSF2/index.html', imgSrc: 'SSF2.png', name: 'SSF2' , 'page': 5, 'id': 992 },
 { href: './html/Thelast.io/index.html', imgSrc: 'thelast.io.png', name: 'Thelast.io' , 'page': 5, 'id': 986 },
 { href: './html/Thats not my neighbor/index.html', imgSrc: 'thats not my neighbor.jpeg', name: 'Thats not my neighbor' , 'page': 5, 'id': 985 },
