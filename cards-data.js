6174975be8b18e967f801f9187f466cc7e24d3d7
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:08 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Sleepy knight/index.html', imgSrc: 'sleepy knight.jpeg', name: 'Sleepy knight' , 'page': 5, 'id': 950 },
   { href: './html/Slow roads/index.html', imgSrc: 'slow roads.jpeg', name: 'Slow roads' , 'page': 5, 'id': 954 },
   { href: './html/Snow Rider 3d/index.html', imgSrc: 'snow rider 3d.jpeg', name: 'Snow Rider 3d' , 'page': 5, 'id': 961 },
   { href: './html/Sonic Revert/index.html', imgSrc: 'sonic revert.jpeg', name: 'Sonic Revert' , 'page': 5, 'id': 965 },
